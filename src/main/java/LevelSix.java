70345987721d35470a2618185a7715ca80366a00
hcy <2320032466@qq.com>
2020-08-16 12:36:59 +0800

    Level-1

--- src/main/java/LevelSix.java
+++ src/main/java/LevelSix.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class UseEnum {
+public class LevelSix {
     public static ArrayList<Task> tasks = new ArrayList<>();
     //static int numberOfTasks = 0;
 
@@ -20,17 +20,17 @@ public class UseEnum {
         while (!next.equals("bye")) {
             //System.out.println(next);
             try {
-                if (parsecommand(next).get("type").equals(Command.LIST)) {
+                if (next.equals("list")) {
                     excecuteList();
-                } else if (parsecommand(next).get("type").equals(Command.DONE)) {
+                } else if (parsecommand(next).get("type").equals("done")) {
                     excecuteDone(parsecommand(next));
-                } else if (parsecommand(next).get("type").equals(Command.DELETE)) {
+                } else if (parsecommand(next).get("type").equals("delete")) {
                     excecuteDelete(parsecommand(next));
-                } else if (parsecommand(next).get("type").equals(Command.ADD_TODO)) {
+                } else if (parsecommand(next).get("type").equals("todo")) {
                     excecuteToDo(parsecommand(next));
-                } else if (parsecommand(next).get("type").equals(Command.ADD_DEADLINE)) {
+                } else if (parsecommand(next).get("type").equals("deadline")) {
                     excecuteDeadline(parsecommand(next));
-                } else if (parsecommand(next).get("type").equals(Command.ADD_EVENT)) {
+                } else if (parsecommand(next).get("type").equals("event")) {
                     excecuteEvent(parsecommand(next));
                 } else {
                     excecuteHandle(new Exception("The command is not found"));
@@ -58,27 +58,27 @@ public class UseEnum {
     public static void excecuteAdd(Task task){
 
 
-        tasks.add(task);
+            tasks.add(task);
 
-        String result = "Got it. I have added this task:\n  " + task.toString() + "\nNow you have " + tasks.size() + " tasks in the list.";
-        System.out.println(Duke.makeBlock(result));
+            String result = "Got it. I have added this task:\n  " + task.toString() + "\nNow you have " + tasks.size() + " tasks in the list.";
+            System.out.println(Duke.makeBlock(result));
 
     }
 
 
-    static void excecuteToDo(HashMap<String, Object> map) {
+    static void excecuteToDo(HashMap<String, String> map) {
         //String result = "Got it. I have added this task:\n";
 
-        excecuteAdd(new ToDo((String) map.get("description")));
+        excecuteAdd(new ToDo(map.get("description")));
     }
 
-    static void excecuteDeadline(HashMap<String, Object> map){
-        excecuteAdd(new Deadline((String) map.get("description"), (String) map.get("time")));
+    static void excecuteDeadline(HashMap<String, String> map){
+        excecuteAdd(new Deadline(map.get("description"), map.get("time")));
     }
 
-    static void excecuteEvent(HashMap<String, Object> map) {
+    static void excecuteEvent(HashMap<String, String> map) {
         //String result = "Got it. I have added this task:\n";
-        excecuteAdd(new Event((String) map.get("description"), (String) map.get("time")));
+        excecuteAdd(new Event(map.get("description"), map.get("time")));
     }
 
     static void excecuteHandle(Exception exception) {
@@ -86,8 +86,8 @@ public class UseEnum {
         System.out.println(Duke.makeBlock(exception.getMessage()));
     }
 
-    public static void excecuteDone(HashMap<String, Object> map) {
-        int count = Integer.parseInt((String) map.get("number"));
+    public static void excecuteDone(HashMap<String, String> map) {
+        int count = Integer.parseInt(map.get("number"));
         if(count > tasks.size() || count <= 0) {
             System.out.println(Duke.makeBlock("There is no such task"));
         } else {
@@ -97,8 +97,8 @@ public class UseEnum {
 
     }
 
-    public static void excecuteDelete(HashMap<String, Object> map) {
-        int count = Integer.parseInt((String) map.get("number"));
+    public static void excecuteDelete(HashMap<String, String> map) {
+        int count = Integer.parseInt(map.get("number"));
         if(count > tasks.size() || count <= 0) {
             System.out.println(Duke.makeBlock("There is no such task"));
         } else {
@@ -114,40 +114,40 @@ public class UseEnum {
         System.out.println(Duke.makeBlock(Duke.EXITMESSAGE));
     }
 
-    public static HashMap<String, Object> parsecommand(String command) throws CommandNotFoundException {
+    public static HashMap<String, String> parsecommand(String command) throws CommandNotFoundException {
 
-        HashMap<String, Object> map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         //map.put("type", "add");
         command = command.strip();
         if (command.equals("bye")) {
             map.put("type", "exit");
         } else if (command.equals("list")) {
-            map.put("type", Command.LIST);
+            map.put("type", "list");
         } else if (command.split(" ")[0].equals("done")) {
-            evaluateCompleteOrDelete(map, command, Command.DONE);
+            evaluateCompleteOrDelete(map, command);
 
         } else if (command.split(" ")[0].equals("delete")) {
-            evaluateCompleteOrDelete(map, command, Command.DELETE);
+            evaluateCompleteOrDelete(map, command);
         }  else if (command.split(" ")[0].equals("todo")) {
             if (command.strip().split(" ").length == 1) {
                 throw new CommandNotFoundException("The description for todo should not be empty");
             } else {
-                map.put("type", Command.ADD_TODO);
+                map.put("type", "todo");
                 map.put("description", command.split("\\s+", 2)[1]);
             }
 
         } else if (command.split(" ")[0].equals("deadline")) {
-            evaluateAddCommand(map, " /by ", command, Command.ADD_DEADLINE);
+            evaluateAddCommand(map, " /by ", command);
 
         } else if (command.split(" ")[0].equals("event")) {
-            evaluateAddCommand(map, " /at ", command, Command.ADD_EVENT);
+            evaluateAddCommand(map, " /at ", command);
         } else {
             throw new CommandNotFoundException("The command is not found");
         }
 
         return map;
     }
-    public static void evaluateAddCommand(HashMap<String, Object> map, String string, String command, Command commandtype) throws CommandNotFoundException {
+    public static void evaluateAddCommand(HashMap<String, String> map, String string, String command) throws CommandNotFoundException {
         if (command.split(" ").length == 1) {
             throw new CommandNotFoundException("The description for event should not be empty");
         } else if (command.split(" ")[0].equals("event") || command.split(" ")[0].equals("deadline")) {
@@ -161,7 +161,7 @@ public class UseEnum {
                 String time;
                 String description = content.split(string)[0];
                 time = content.strip().split(string, 2)[1];
-                map.put("type", commandtype);
+                map.put("type", command.split(" ")[0]);
                 map.put("description", description);
                 map.put("time", time);
             }
@@ -170,7 +170,7 @@ public class UseEnum {
         }
     }
 
-    public static void evaluateCompleteOrDelete(HashMap<String, Object> map, String command, Command commandType) throws CommandNotFoundException {
+    public static void evaluateCompleteOrDelete(HashMap<String, String> map, String command) throws CommandNotFoundException {
         String type = command.split(" ")[0];
         //System.out.println(type);
         try {
@@ -179,7 +179,7 @@ public class UseEnum {
                 throw new CommandNotFoundException("The " + type + " command needs to contain the number label of the task to be completed");
             }
             int number = Integer.parseInt(command.split("\\s+", 2)[1]);
-            map.put("type", commandType);
+            map.put("type", type);
             map.put("number", String.valueOf(number));
 
         } catch (NumberFormatException exception) {
